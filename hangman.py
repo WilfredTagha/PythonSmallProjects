86cacc74652d7d066a08710487d94656082d18c1
Tagha Wilfred <taghawilfred@gmail.com>
2022-10-17 20:03:46 +0200

    completed the game for testing

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+word_list = ['elephant', 'giraffe', 'tiger','crocodie','leopard','sheep','goat','pig'] 
 stages = [
     '''
       +---+
@@ -57,5 +58,31 @@ stages = [
     =========   
     '''
 ]
-print(stages[6])
+import random
+chosen_word = random.choice(word_list)
+
+
+display = []
+for _ in range(len(chosen_word)):
+    display += "_"
+game_end = False
+lives = 6
+while not game_end:
+    guess = input("Guess a letter: ").lower()
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("you loose")
+            game_end = True
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if "_" not in display:
+        print("You win")
+        game_end = True
+    
+    print(stages[lives])
+    print(display)
+
 
